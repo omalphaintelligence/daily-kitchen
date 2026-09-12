d9d1255614d5689808b1e60ab07c5c64dbac8072
omalphaintelligence <mishraommail@gmail.com>
2026-09-12 09:22:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tdk-v1";
+const CACHE = "tdk-v2";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
